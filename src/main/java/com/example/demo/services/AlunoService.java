3cd3403b8595e6da021d548746a5106768bd4692
Gervásio Neto <gervasiolopesn@gmail.com>
2025-08-26 18:59:58 -0300

    feat: Aluno repository and service (Aula 26-08-25)

---
+++ src/main/java/com/example/demo/services/AlunoService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import com.example.demo.entity.Aluno;
+import com.example.demo.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    private List<Aluno> findAllAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    private Optional<Aluno> findById(Long id){
+        return alunoRepository.findById(id);
+    }
+
+    private Aluno saveAluno(Aluno aluno){
+        return alunoRepository.save(aluno);
+    }
+
+    private void deleteAluno(Long id){
+        alunoRepository.deleteById(id);
+    }
+
+    private Aluno updateAluno(Long id, Aluno updatedAluno){
+        return alunoRepository.findById(id).map(aluno -> {
+            aluno.setNome(updatedAluno.getNome());
+            aluno.setEmail(updatedAluno.getEmail());
+            aluno.setSenha(updatedAluno.getSenha());
+            aluno.setDataNascimento(updatedAluno.getDataNascimento());
+            return alunoRepository.save(aluno);
+        }).orElseThrow(() -> new RuntimeException("Aluno não existe"));
+    }
+}
