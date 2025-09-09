c0e3fbc327a51552655bc0f1e6d1b09b296d313d
Gervásio Neto <gervasiolopesn@gmail.com>
2025-09-09 18:57:31 -0300

    changes for AlunoService AULA 09/09/25

--- src/main/java/com/example/demo/services/AlunoService.java
+++ src/main/java/com/example/demo/services/AlunoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.AlunoDTO;
 import com.example.demo.entity.Aluno;
 import com.example.demo.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,29 +15,40 @@ public class AlunoService {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    public List<Aluno> findAllAlunos(){
-        return alunoRepository.findAll();
+    public List<AlunoDTO> findAllAlunos(){
+        return alunoRepository.findAll()
+                .stream()
+                .map(this::toDTO)
+                .toList();
     }
 
-    public Optional<Aluno> findById(Long id){
-        return alunoRepository.findById(id);
+    public Optional<AlunoDTO> findById(Long id){
+        return alunoRepository.findById(id).map(this::toDTO);
     }
 
-    public Aluno saveAluno(Aluno aluno){
-        return alunoRepository.save(aluno);
+    public AlunoDTO saveAluno(Aluno aluno){
+        Aluno alunoSaved = alunoRepository.save(aluno);
+        return toDTO(alunoSaved);
     }
 
     public void deleteAluno(Long id){
         alunoRepository.deleteById(id);
     }
 
-    public Aluno updateAluno(Long id, Aluno updatedAluno){
+    public AlunoDTO updateAluno(Long id, Aluno updatedAluno){
         return alunoRepository.findById(id).map(aluno -> {
             aluno.setNome(updatedAluno.getNome());
             aluno.setEmail(updatedAluno.getEmail());
             aluno.setSenha(updatedAluno.getSenha());
             aluno.setDataNascimento(updatedAluno.getDataNascimento());
-            return alunoRepository.save(aluno);
+
+            Aluno alunoUpdated = alunoRepository.save(aluno);
+            return toDTO(alunoUpdated);
         }).orElseThrow(() -> new RuntimeException("Aluno não existe"));
+    }
+
+    //Mapper
+    private AlunoDTO toDTO(Aluno aluno){
+        return new AlunoDTO(aluno.getId(), aluno.getNome(), aluno.getEmail());
     }
 }
